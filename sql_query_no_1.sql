c66cf9474792089dc3da63daec75dd8a63c50a95
Abdullah Mamun <118166641+Abdullah723669-cmk@users.noreply.github.com>
2025-05-13 23:29:29 +0600

    Update sql_query_no_1.sql

--- sql_query_no_1.sql
+++ sql_query_no_1.sql
@@ -10,15 +10,16 @@ CREATE TABLE biker (
     admition_date DATE
 );
 -- Show first 10 rows from student.biker table
-SELECT * FROM viker LIMIT 10;
+SELECT * FROM student.biker LIMIT 10;
 -- Counts how many biker study in the Science department.
-SELECT COUNT(*) FROM biker WHERE department = 'Science';
+SELECT COUNT(*) FROM student.biker WHERE department = 'Science';
 -- Counts how many biker are in each department.
 SELECT department, COUNT(*) AS total_biker
-FROM biker
+FROM student.biker
 GROUP BY department;
 -- Show all student from student.biker table
 SELECT * FROM student.biker;
+-- Show specific biker information/data by id
 SELECT * FROM student.biker where id=2;
 -- Sorts student.biker by fees in descending order.
 SELECT * FROM student.biker ORDER BY fees DESC; 
